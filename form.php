4a7ca7824af77c9397f2cb48177da3f949cbd68e
yatoyataro <168419749+yatoyataro@users.noreply.github.com>
2024-07-08 18:18:51 +0800

    Update form.php

--- form.php
+++ form.php
@@ -1,5 +1,5 @@
 <?php
-    if (isset($_POST['username']) && isset($_POST['password'])) {
+    if (isset($_POST['insert'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
 
